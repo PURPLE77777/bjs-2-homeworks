e6d1fcd77c5eaf66c442d9c2d5a9846fc6132a6b
56859 <C:\Users\User\AppData\Roaming\The Bat!>
2023-04-29 15:04:07 +0300

    Подготовка третьего

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length != arr2.length) return false;
+    return arr1.every((num, ind) => {
+        return num == arr2[ind];
+    });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+    const usersAgeByGender = users.map((user) => {
+        if(user.gender == gender) return user.age;
+    });
+    return usersAgeByGender.reduce((avg, val, _, { length }) => {avg + val / length}, 0);
 }
