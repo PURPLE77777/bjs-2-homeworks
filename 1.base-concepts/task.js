03ef4454be572451a10cc31e2357f6c5da1a2b2a
56859 <C:\Users\User\AppData\Roaming\The Bat!>
2023-04-29 12:29:00 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,7 +4,7 @@ function solveEquation(a, b, c) {
   let discriminant = Math.pow(b, 2) - 4 * a * c;
 
   if (discriminant == 0) {
-    arr.push(-b / (2 * a))
+    arr.push(-b / (2 * a));
   } else if (discriminant > 0) {
     let firstRoot = (-b + Math.sqrt(discriminant) ) / (2 * a),
       secondRoot = (-b - Math.sqrt(discriminant) ) / (2 * a);
@@ -15,15 +15,15 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent = Number(percent) / 100 / 12
-  contribution = Number(contribution)
-  amount = Number(amount)
-  countMonths = Number(countMonths)
+  percent = Number(percent) / 100 / 12;
+  contribution = Number(contribution);
+  amount = Number(amount);
+  countMonths = Number(countMonths);
   
   if (![percent, contribution, amount, countMonths].every((item) => {
-    return typeof item == 'number' && !isNaN(item)
+    return typeof item == 'number' && !isNaN(item);
   })) return false;
-  
+
   let creditSum = (amount - contribution) * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
 
   return Math.round(creditSum * countMonths * 100) / 100;
