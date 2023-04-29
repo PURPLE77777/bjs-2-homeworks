cce1fd9c8a06fbf18422333d49a1ef2273cc6ecf
56859 <C:\Users\User\AppData\Roaming\The Bat!>
2023-04-29 14:09:04 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,68 @@
 function getArrayParams(...arr) {
+	if (!arr.length) return 0;
 
-  return { min: min, max: max, avg: avg };
+	let max = Math.max(...arr),
+		min = Math.min(...arr),
+		sum = arr.reduce((prevValue, curValue) => {
+			return prevValue + curValue;
+		});
+
+	return {
+		min: min,
+		max: max,
+		avg: Number((sum / arr.length).toFixed(2))
+	};
 }
 
 function summElementsWorker(...arr) {
-
+	return arr.reduce((prevValue, curValue) => {
+		return prevValue + curValue;
+	}, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
+	if (!arr.length) return 0;
 
+	return Math.max(...arr) - Math.min(...arr);
 }
 
 function differenceEvenOddWorker(...arr) {
+	if (!arr.length) return 0;
 
+	let sumEvenElement = 0,
+		sumOddElement = 0;
+
+	for (let num of arr) {
+		if (num % 2 == 0) sumEvenElement += num;
+		else sumOddElement += num;
+	}
+
+	return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+	if (!arr.length) return 0;
 
+	let sumEvenElement = 0,
+		countEvenElement = 0;
+
+	for (let num of arr) {
+		if (num % 2 == 0) {
+			sumEvenElement += num;
+			countEvenElement++;
+		}
+	}
+
+	return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
 
+	for (const arr of arrOfArr) {
+		const resultOfFunc = func(...arr);
+		if (maxWorkerResult < resultOfFunc) maxWorkerResult = resultOfFunc;
+	}
+
+	return maxWorkerResult;
 }
