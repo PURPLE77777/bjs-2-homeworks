4a85830bcb107bfc3d6e7f57baba1410ab034a57
PURPLE77777 <stefanovich.pasha@yandex.by>
2023-04-29 16:49:00 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,20 @@
 function compareArrays(arr1, arr2) {
-    if (arr1.length != arr2.length) return false;
-    return arr1.every((num, ind) => {
-        return num == arr2[ind];
-    });
+	if (arr1.length != arr2.length) return false;
+	return arr1.every((num, ind) => {
+		return num == arr2[ind];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+	const usersAgesByGender = users
+		.filter(user => {
+			return user.gender == gender;
+		})
+		.map(user => {
+			return user.age;
+		});
 
-    const usersAgeByGender = users.map((user) => {
-        if(user.gender == gender) return user.age;
-    });
-    return usersAgeByGender.reduce((avg, val, _, { length }) => {avg + val / length}, 0);
+	return usersAgesByGender.reduce((avg, val, _, { length }) => {
+		return avg + val / length;
+	}, 0);
 }
